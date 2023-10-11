699148b25e82acea57b6bd1487a7e6b5fe33f39e
Carolyn Au <cyin@google.com>
2023-10-11 09:31:15 -0700

    tmp updates

--- experimental/sdg-tracker-un/frontend-tester/UNSD-Website-skeleton/UNSD.Website/ClientApp/src/templates/DataCommons/components/shared/components.tsx
+++ experimental/sdg-tracker-un/frontend-tester/UNSD-Website-skeleton/UNSD.Website/ClientApp/src/templates/DataCommons/components/shared/components.tsx
@@ -120,15 +120,22 @@ export const SearchBar: React.FC<{
   initialQuery?: string;
   isSearching?: boolean;
   onSearch: (query: string) => void;
+  onFocus?: () => void;
+  clearInputOnFocus?: boolean;
   placeholder?: string;
-}> = ({ initialQuery, isSearching, onSearch, placeholder }) => {
+}> = ({ initialQuery, isSearching, onSearch, onFocus, clearInputOnFocus, placeholder }) => {
   const [query, setQuery] = useState("");
 
+  console.log("initial query", query);
   useEffect(() => {
     if (initialQuery) {
+      console.log("updatig initial query: ", initialQuery);
       setQuery(initialQuery);
     }
   }, [initialQuery]);
+
+  console.log("initial query", query);
+  console.log("query", query);
 
   return (
     <SearchInputContainer>
@@ -145,6 +152,12 @@ export const SearchBar: React.FC<{
           value={query}
           disabled={isSearching}
           onChange={(e) => setQuery(e.currentTarget.value)}
+          onFocus={() => {
+            if (clearInputOnFocus) {
+              setQuery("");
+            }
+            onFocus && onFocus()
+          }}
           onPressEnter={() => {
             onSearch(query);
           }}
